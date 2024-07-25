23f73456111aa72201159320d4ac780feb3dfb09
julianfssen <julianfssen@gmail.com>
2024-07-25 08:50:39 +0800

    Add logic to request SOL airdrop on devnet

---
+++ airdrop.ts
@@ -0,0 +1,23 @@
+import { Connection, Keypair, LAMPORTS_PER_SOL } from "@solana/web3.js";
+import wallet from "./dev-wallet.json";
+
+const DEVNET_RPC_URL = "https://api.devnet.solana.com";
+
+try {
+  const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
+  const connection = new Connection(DEVNET_RPC_URL);
+  const solToClaim = 2;
+
+  const airdrop = async () => {
+    try {
+      const txhash = await connection.requestAirdrop(keypair.publicKey, solToClaim * LAMPORTS_PER_SOL);
+      console.log(`Success! Check out your TX here: https://explorer.solana.com/tx/${txhash}?cluster=devnet`);
+    } catch (err) {
+      console.error("Failed to request airdrop", err);
+    }
+  };
+
+  airdrop();
+} catch (err) {
+  console.error("Failed to request airdrop", err);
+}
